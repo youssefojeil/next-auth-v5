728ef63acd53223c836dd4b9d56641b95a04f656
Youssef <youssef.ojeil@hotmail.com>
2024-01-02 21:51:04 -0500

    create user on register

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
 
-import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
+
+import { db } from '@/lib/db';
+import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   console.log(values);
@@ -13,6 +16,32 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: 'Invalid fields!',
     };
   }
+
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      error: 'Email already in use!',
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
 
   return {
     success: 'Account Created!',
