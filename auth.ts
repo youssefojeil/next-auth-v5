0645ffe2a990cab38cc29929f16738bd83360a52
Youssef <youssef.ojeil@hotmail.com>
2024-01-03 19:01:16 -0500

    set up nextauth callbacks for jwt and session, attach userid to session

--- auth.ts
+++ auth.ts
@@ -10,6 +10,20 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ sessionToken: token, session });
+
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
