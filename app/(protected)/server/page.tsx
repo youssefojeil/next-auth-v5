5f7c5297a5af0d8987a268e88c62a5adbe07f468
Youssef <youssef.ojeil@hotmail.com>
2024-01-05 12:31:27 -0500

    create navbar and protected route for current user with menu and pages for server, client admin and settings

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,9 @@
+import { currentUser } from '@/lib/auth';
+
+const ServerPage = async () => {
+  const user = currentUser();
+
+  return <div>{JSON.stringify(user)}</div>;
+};
+
+export default ServerPage;
